485382a13ad4f5967a78d479bd0d05883ccbd7d3
Replit user <>
2024-02-02 19:35:23 +0000

    conflicts

--- backend/app/runs.py
+++ backend/app/runs.py
@@ -54,12 +54,7 @@ async def _run_input_and_config(request: Request,
                                 opengpts_user_id: OpengptsUserId, payload):
   try:
     body = None
-<<<<<<< HEAD
     if not hasattr(payload, "Config"):
-=======
-    print(payload)
-    if payload:
->>>>>>> 80af016 (import os; add print to debug)
       body = payload
     else:
       body = await request.json()
@@ -145,12 +140,8 @@ async def stream_run(
   config["callbacks"] = [streamer, event_aggregator]
 
   body = None
-<<<<<<< HEAD
+
   if not hasattr(payload, "Config"):
-=======
-  print(payload)
-  if payload:
->>>>>>> 80af016 (import os; add print to debug)
     body = payload
   else:
     body = await request.json()
